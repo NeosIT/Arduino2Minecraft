cba414fa29d1d367784b0ce37f35e6cccef61a5e
Stefan Fiedler <fiedlerstefan93@gmail.com>
2016-05-26 13:02:22 +0200

    * temp. fix für die Verwendung eines Potis

--- src/main/java/de/neosit/minecraft/arduinointegration/CommandInterface.java
+++ src/main/java/de/neosit/minecraft/arduinointegration/CommandInterface.java
@@ -9,37 +9,52 @@ import org.bukkit.scheduler.BukkitScheduler;
 
 public class CommandInterface {
 	public static final String PREFIX = "arduino_";
-	
+
 	private Queue<String> queue = new LinkedList<String>();
 	private Configuration config;
-	
+
 	public CommandInterface(Configuration config) {
 		this.config = config;
 	}
 
 	public void addArduinoValue(String arduinoValue) {
-		String command = config.getCommandForArduinoValue(arduinoValue);
-		addCommand(command);
+		try {
+			String command = "";
+			if (arduinoValue.startsWith("poti:")) {
+				String value = arduinoValue.substring(arduinoValue.lastIndexOf(":") + 1);
+				System.out.println("arduino_poti: " + value);
+				command = config.getCommandForArduinoValue("poti");
+				command = command.replace("?", value);
+			} else {
+				command = config.getCommandForArduinoValue(arduinoValue);
+			}
+			System.out.println("command: " + command);
+
+			addCommand(command);
+		} catch (Exception e) {
+			System.out.println("command error" + e.getMessage());
+		}
+
 	}
-	
+
 	public void addCommand(String command) {
 		queue.add(command);
 	}
-	
+
 	public void register(JavaPlugin plugin) {
 		BukkitScheduler scheduler = Bukkit.getScheduler();
 		Runnable run = new Runnable() {
-			
+
 			public void run() {
 				if (queue.isEmpty()) {
 					return;
 				}
-				
+
 				String command = queue.remove();
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
 			}
 		};
-		
+
 		scheduler.scheduleSyncRepeatingTask(plugin, run, 0, config.getPeriod());
 	}
 }
